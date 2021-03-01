37a905ebf343feb547565243f90ffa44196abc58
francescapetra <francescalaurapetra@gmail.com>
2021-03-01 17:50:24 +0100

    creazione nuovi messaggi alert

--- dadi/script-2.js
+++ dadi/script-2.js
@@ -6,13 +6,13 @@ var computer = Math.floor(Math.random( ) *6 )+ 1;
 console.log(computer);
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
 if (giocatore > computer) {
-  alert("ha vinto giocatore");
+  alert("ha vinto giocatore con il numero: " + giocatore );
 }
 else if (computer> giocatore) {
-  alert("ha vinto computer");
+  alert("ha vinto computer con il numero: " + computer );
 }
 else {
-  alert("Siete pari");
+  alert("Siete pari con il numero: " + giocatore );
 }
 // Prima di partire a scrivere codice poniamoci qualche domanda:
 // Che ci sia un array da qualche parte?
